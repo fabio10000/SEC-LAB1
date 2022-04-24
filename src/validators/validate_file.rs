b490d72e58be26f59f5f177df0151b0045e8a3a8
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2022-04-24 19:53:51 +0200

    unit tests

--- src/validators/validate_file.rs
+++ src/validators/validate_file.rs
@@ -8,19 +8,55 @@ pub fn validate_file(file: &[u8], path: &String) -> bool {
     }
 
     let kind = infer::get(&file).expect("file type is known");
-    return match Path::new(path).extension().unwrap().to_str().unwrap() {
-      "jpeg" => kind.extension() == "jpg",
-      "tif" => kind.extension() == "tiff",
-      extension => kind.extension() == extension
+
+    // translate special extensions supported by infer
+    let extension = match Path::new(path).extension().unwrap().to_str().unwrap() {
+      "jpeg" | "jpe" => "jpg",
+      "tiff" => "tif",
+      "qt" => "mov",
+      other => other
     };
+
+    return extension == kind.extension()
 }
 
-// TODO : implement unit testing
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+  use crate::validators::validate_file;
+
+  #[test]
+  fn it_should_validate_images_and_videos() {
+    // source: https://en.wikipedia.org/wiki/List_of_file_signatures
+    // magic bytes
+    let jpeg = &[0xFF, 0xD8, 0xFF];
+    let webm = &[0x1A, 0x45, 0xDF, 0xA3];
+    let xml = b"<?xml";
+
+    assert!(validate_file(webm, &"example.webm".to_string()));
+    assert!(validate_file(jpeg, &"example.jpg".to_string()));
+
+    // not an image or video file
+    assert!(!validate_file(xml, &"example.xml".to_string()));
+
+    // file format not supported
+    let mpeg_video = &[0x00, 0x00, 0x01, 0xB3];
+    assert!(!validate_file(mpeg_video, &"example.mpeg".to_string()));
+  }
+
+  #[test]
+  fn mime_type_should_match_extension() {
+    // source: https://en.wikipedia.org/wiki/List_of_file_signatures
+    // magic bytes
+    let jpeg = &[0xFF, 0xD8, 0xFF];
+    let png = &[0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+
+    // with multiple extensions
+    assert!(validate_file(jpeg, &"example.jpg".to_string()));
+    assert!(validate_file(jpeg, &"example.jpeg".to_string()));
+    assert!(!validate_file(jpeg, &"example.png".to_string()));
+
+    assert!(validate_file(png, &"example.png".to_string()));
+    assert!(!validate_file(png, &"example.tif".to_string()));
+ 
+  }
 }
--- src/validators/validate_uuid.rs
+++ src/validators/validate_uuid.rs
@@ -12,12 +12,26 @@ pub fn validate_uuid(uuid_str: &String) -> bool {
   return RE.is_match(uuid_str);
 }
 
-// TODO : implement unit testing
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+  use crate::validators::validate_uuid;
+
+  #[test]
+  fn it_should_be_a_valid_uuid() {
+    assert!(!validate_uuid(&"asdasdasdd".to_string()));
+    assert!(!validate_uuid(&"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa".to_string()));
+    assert!(validate_uuid(&"5301963a-065a-5962-99d2-85846dd846ef".to_string()));
+  }
+
+  #[test]
+  fn it_should_reject_wrong_versions() {
+    // v1
+    assert!(!validate_uuid(&"3518e3fe-c3e6-11ec-9d64-0242ac120002".to_string()));
+    // v3
+    assert!(!validate_uuid(&"f0df6355-29fa-36c4-89e4-c9a80042c866".to_string()));
+    // v4
+    assert!(!validate_uuid(&"e4e14ea4-dbaf-4726-a666-1333f6922b00".to_string()));
+    // v5
+    assert!(validate_uuid(&"5301963a-065a-5962-99d2-85846dd846ef".to_string()));
+  }
 }
